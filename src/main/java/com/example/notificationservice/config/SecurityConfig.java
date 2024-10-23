362b03e716aeeed914e267a19112437833b79cd5
lucianvaida <lucian.vaida.ext@ausy-technologies.de>
2024-10-23 21:16:33 +0300

    security configs

--- src/main/java/com/example/notificationservice/config/SecurityConfig.java
+++ src/main/java/com/example/notificationservice/config/SecurityConfig.java
@@ -8,12 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
 @EnableWebSecurity
@@ -21,21 +16,6 @@ import org.springframework.security.web.session.HttpSessionEventPublisher;
 @AllArgsConstructor
 class SecurityConfig {
     private final GraphQlProperties graphQlProperties;
-
-    @Bean
-    public SessionRegistry sessionRegistry() {
-        return new SessionRegistryImpl();
-    }
-
-    @Bean
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(sessionRegistry());
-    }
-
-    @Bean
-    public HttpSessionEventPublisher httpSessionEventPublisher() {
-        return new HttpSessionEventPublisher();
-    }
 
     @Bean
     public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
